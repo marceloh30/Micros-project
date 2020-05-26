9d7a44c2a4af59d686d7fa3b50e142e8633a9543
marceloh30 <38164007+marceloh30@users.noreply.github.com>
2020-05-26 19:56:39 -0300

    Agregado de SERIALMAX
    para dejarlo en funcion de esa macro el envio y la recepcion

--- Obligatorio1.X/variablesGlobales.h
+++ Obligatorio1.X/variablesGlobales.h
@@ -14,6 +14,7 @@
 #define LARGO_PRECIO 2      //Largo en EEPROM de precio de c/art.
 #define CANT_ART 13         //Cantidad de artículos
 #define PRECIOMAX 999       //Valor maximo de la cuenta
+#define SERIALMAX 10 
 
 //Defino variables externas (debido a que cruzan archivos y funciones)
 extern unsigned short int cuenta, auxCuenta;
@@ -22,11 +23,11 @@ extern char serial;
 extern char modoDebug;
 extern short int productoIngresado;
 extern short int numProd;
-extern char codigoEntrada[10];
+extern char codigoEntrada[SERIALMAX];
 extern unsigned char ventasLote;
 extern unsigned short int montosLote;
 extern char nroLote;
-extern unsigned char prodIngresados[13]; //Para guardar datos/uso de 99 prod. en 99 bits!
+extern unsigned char prodIngresados[13]; //13 bytes para guardar datos/uso de 99 prod. en 99 bits!
 
 #endif	/* VARIABLESGLOBALES_H */
 
