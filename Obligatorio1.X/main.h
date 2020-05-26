d15381ada9b88c8feca3dee47cb1da983e6b24f2
marceloh30 <38164007+marceloh30@users.noreply.github.com>
2020-05-26 19:10:31 -0300

    header main.h
    Ta mal??

--- Obligatorio1.X/main.h
+++ Obligatorio1.X/main.h
@@ -1,1 +1,36 @@
+// Seteo de Bits de Configuración del PIC:
+#pragma config FOSC = XT        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+//Macros:
+#define _XTAL_FREQ 4000000  //Frec. de Cristal que usamos
+#define LARGO_ART 8         //Largo de cada artículo recibido(2-TP, 6-Codigo)
+#define LARGO_PRECIO 2      //Largo en EEPROM de precio de c/art.
+#define CANT_ART 13         //Cantidad de artículos
+
+#include <xc.h>
+#include "variablesGlobales.h"
+#include "mostrarInicializar.h"
+#include "manejarProductos.h"
+#include "lectura.h"
+#include "acciones.h"
+//Defino variables estaticas (debido a que cruzan funciones)
+unsigned short int cuenta, auxCuenta;
+short int huboInt = 0;
+char serial = 0;
+char modoDebug = 0;
+short int productoIngresado;
+short int numProd;
+char codigoEntrada[10];
+char ventasLote = 0;
+unsigned short int montosLote = 0;
+char nroLote = 1;
+unsigned char prodIngresados[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0}; //99 lugares para 99 productos (1 en c/bit)
+
 
