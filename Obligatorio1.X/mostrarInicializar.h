db066c38a3fbb7b68fda5676e67e01d3cfca9f60
marceloh30 <38164007+marceloh30@users.noreply.github.com>
2020-05-26 23:57:08 -0300

    Cambio de includes
    Ver si sacar el BMS de aca jeje

--- Obligatorio1.X/mostrarInicializar.h
+++ Obligatorio1.X/mostrarInicializar.h
@@ -1,12 +1,28 @@
 /* 
  * File:   mostrarInicializar.h
- * Author: julia
+ * Authors: Julian Ferreira y Marcelo Hernandez
  *
  * Created on 26 de mayo de 2020, 12:24 PM
  */
 
 #ifndef MOSTRARINICIALIZAR_H
 #define	MOSTRARINICIALIZAR_H
+
+#include <xc.h>
+#include "variablesGlobales.h"
+
+static unsigned const char BMS[] = {
+    0b00000000,
+    0b00010000,
+    0b00100000,
+    0b00110000,
+    0b01000000,
+    0b01010000,
+    0b01100000,
+    0b01110000,
+    0b10000000,
+    0b10010000,
+};
 
 void mostrarDigitos(unsigned int num);
 
