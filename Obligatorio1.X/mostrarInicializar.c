61619c2a51cf6f3aba7d952f35f93fcf08c69bec
marceloh30 <38164007+marceloh30@users.noreply.github.com>
2020-05-26 23:57:33 -0300

    Cambio de includes
    Y del bms..

--- Obligatorio1.X/mostrarInicializar.c
+++ Obligatorio1.X/mostrarInicializar.c
@@ -1,18 +1,5 @@
 #include "mostrarInicializar.h"
 
-static unsigned const char BMS[] = {
-    0b00000000,
-    0b00010000,
-    0b00100000,
-    0b00110000,
-    0b01000000,
-    0b01010000,
-    0b01100000,
-    0b01110000,
-    0b10000000,
-    0b10010000,
-};
-
 void mostrarDigitos(unsigned int num) { //num debe ser el numero entero (por ej. 99,1 --> 99)
     //Verifico si puedo redondear el numero 
     PORTB = BMS[num/100]; // Tomo las decenas
