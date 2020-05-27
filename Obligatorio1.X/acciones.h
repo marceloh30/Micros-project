48d25c3b9d70441a0252cdc24f420d38512151e3
marceloh30 <38164007+marceloh30@users.noreply.github.com>
2020-05-27 00:17:57 -0300

    cambio de includes

--- Obligatorio1.X/acciones.h
+++ Obligatorio1.X/acciones.h
@@ -1,13 +1,14 @@
-/* 
- * File:   acciones.h
- * Author: julian
- *
- * Created on 26 de mayo de 2020, 12:15 PM
- */
-
 #ifndef ACCIONES_H
 #define	ACCIONES_H
 
+//Librerias
+#include <xc.h>
+#include "lectura.h"
+#include "manejarProductos.h"
+#include "mostrarInicializar.h"
+#include "variablesGlobales.h"
+
+//Firmas
 void accionesAceptar(void);
 
 void accionesDeshacer(void);
